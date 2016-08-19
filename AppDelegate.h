ee4b49d2dd7c5dbab6e7764f8429f67e4b790a82
Leklef <lenar8553@gmail.com>
2016-08-19 10:48:52 +0300

    1

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Forget-Me-Not
+//
+//  Created by Ленар on 19.08.16.
+//  Copyright © 2016 LeNComp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Forget-Me-Not
+//
+//  Created by Ленар on 19.08.16.
+//  Copyright © 2016 LeNComp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
